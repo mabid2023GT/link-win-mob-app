ab5e103f6a1a07914e7784347f0b94aa0f68d0dc
mohammadAbid0 <mohammad.abid@nextryapp.com>
2024-08-23 11:44:42 +0300

    set up the run script with all the flutter confoguration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:link_win_mob_app/core/config/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,60 +13,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'LinkWin',
+      debugShowCheckedModeBanner: false,
+      theme: kAppThemeData,
+      home: const Scaffold(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+final ThemeData kAppThemeData = _buildAppTheme();
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+ThemeData _buildAppTheme() {
+  final base = ThemeData.light();
+  final baseTextTheme = GoogleFonts.poppinsTextTheme(base.textTheme);
+  return base.copyWith(
+    scaffoldBackgroundColor: kWhite,
+    textTheme: baseTextTheme.copyWith(
+      // Title
+      displayLarge: baseTextTheme.displayLarge!.copyWith(
+        height: 58.5 / 39,
+        fontWeight: FontWeight.w700,
+        fontSize: 39.0,
+        color: kBlack,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      // H1
+      displayMedium: baseTextTheme.displayMedium!.copyWith(
+        height: 46.88 / 31.25,
+        fontWeight: FontWeight.w700,
+        fontSize: 31.25,
+        color: kBlack,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      // H2
+      displaySmall: baseTextTheme.displaySmall!.copyWith(
+        height: 37.5 / 25.0,
+        fontWeight: FontWeight.w400,
+        fontSize: 25.0,
+        color: kBlack,
+      ),
+      // H3, Button Large
+      headlineSmall: baseTextTheme.headlineSmall!.copyWith(
+        height: 30.0 / 20.0,
+        fontWeight: FontWeight.w400,
+        fontSize: 20.0,
+        color: kBlack,
+      ),
+      // Body Regular (Body Copy), Button Regular
+      bodyLarge: baseTextTheme.bodyLarge!.copyWith(
+        color: kBlack,
+        fontWeight: FontWeight.w400,
+      ),
+      // Body Small
+      bodySmall: baseTextTheme.bodySmall!.copyWith(
+        height: 21.0 / 14.0,
+        fontWeight: FontWeight.w700,
+        fontSize: 14.0,
+        color: kBlack,
+      ),
+      // Caption
+      labelSmall: baseTextTheme.labelSmall!.copyWith(
+        height: 19.2 / 12.8,
+        fontWeight: FontWeight.w400,
+        fontSize: 12.8,
+        color: kCaption,
+      ),
+    ),
+  );
 }
