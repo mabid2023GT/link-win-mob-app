459bee2b6b27a09def2ca58f31fab587533fc1b9
mohammadAbid0 <mohammad.abid@nextryapp.com>
2024-10-04 21:35:40 +0300

    implementing Auth Page

--- lib/features/auth/widget/sign_up.dart
+++ lib/features/auth/widget/sign_up.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:link_win_mob_app/core/config/colors.dart';
+import 'package:link_win_mob_app/responsive_ui_tools/widgets/layout_builder_child.dart';
 import 'package:link_win_mob_app/widgets/link_win_text_field_widget.dart';
 
 class SignUp extends ConsumerWidget {
@@ -14,6 +16,7 @@ class SignUp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Size txtFieldSize = Size(size.width, size.height * 0.15);
+    Size signInSize = Size(size.width * 0.4, size.height * 0.15);
 
     return Scaffold(
       backgroundColor: transparent,
@@ -31,6 +34,10 @@ class SignUp extends ConsumerWidget {
             _emailField(txtFieldSize),
             _space(),
             _passwordField(txtFieldSize),
+            _space(),
+            _termsAndConditions(txtFieldSize),
+            _space(),
+            _signUp(signInSize)
           ],
         ),
       ),
@@ -128,4 +135,128 @@ class SignUp extends ConsumerWidget {
       onChanged: (val) {},
     );
   }
+
+  _signUp(Size size) {
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kHeaderColor,
+        ),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: kBlack,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _termsAndConditions(Size size) {
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child: _TermsAndConditions(),
+    );
+  }
+}
+
+class _TermsAndConditions extends StatefulWidget {
+  const _TermsAndConditions();
+
+  @override
+  State<_TermsAndConditions> createState() => _TermsAndConditionsState();
+}
+
+class _TermsAndConditionsState extends State<_TermsAndConditions> {
+  bool _isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilderChild(
+      child: (minSize, maxSize) {
+        Size checkBoxSize = Size(maxSize.width * 0.1, maxSize.height);
+        double space = maxSize.width * 0.1;
+        Size labelSize =
+            Size(maxSize.width - checkBoxSize.width - space, maxSize.height);
+        return SizedBox(
+          width: maxSize.width,
+          height: maxSize.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: checkBoxSize.width,
+                height: checkBoxSize.height,
+                child: _checkBox(),
+              ),
+              Container(
+                width: labelSize.width,
+                height: labelSize.height,
+                alignment: AlignmentDirectional.center,
+                child: _termsAndConditions(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  _checkBox() {
+    return Checkbox(
+      value: _isChecked,
+      onChanged: (bool? newValue) {
+        setState(
+          () {
+            _isChecked = newValue ?? false;
+          },
+        );
+      },
+    );
+  }
+
+  _termsAndConditions() {
+    return Expanded(
+      child: RichText(
+        // Ensure the text wraps
+        softWrap: true,
+        text: TextSpan(
+          // Adding space between lines globally
+          style: TextStyle(
+            height: 1.5,
+            fontSize: 16,
+          ),
+          children: [
+            _blackText('I accept the '),
+            _blueText('Terms & Conditions', () {}),
+            _blackText(' and '),
+            _blueText('Privacy Policy', () {}),
+            _blackText('.'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _blackText(String text) => TextSpan(
+        text: text,
+        style: TextStyle(
+          color: kBlack,
+        ),
+      );
+
+  _blueText(String text, VoidCallback onTap) => TextSpan(
+        text: text,
+        style: TextStyle(
+          color: kBlue,
+          decoration: TextDecoration.underline,
+        ),
+        recognizer: TapGestureRecognizer()..onTap = onTap,
+      );
 }
